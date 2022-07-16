879c825b8b1a326f6ca97e7ae409302ce49d4929
ahmad <ahmadsorann@gmail.com>
2022-07-16 15:40:02 +0300

    final

--- src/app/My-Projects.js
+++ src/app/My-Projects.js
@@ -99,6 +99,71 @@ export const MyProjects = [
             'https://res.cloudinary.com/ahmacloud/image/upload/v1657921591/portfolio/Screenshot_from_2022-07-16_00-45-05_qio0od.png'
 
         ]
+    },
+    {
+        id: "Tasbihat",
+        title: "Tasbihat App",
+        description: 'Tasbihat React Native App',
+        image: 'https://res.cloudinary.com/ahmacloud/image/upload/v1652020663/portfolio/wv50xr1v0znq5xeyyood.png',
+        web: 'https://tasbihat.en.uptodown.com/android',
+        github: 'https://github.com/ahmadsoran/tasbihat-reactnative-andriod-ios',
+        longDescription: 'Tasbih counting app made with react native for android and ios, also you can listen & read Quran with their 3 lang tafsir, adding custom zikrs & counting time & wallpaper   the application available for the android user on uptodown app market.',
+        otherImages: [
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1652894320/71FB34D7-38DC-4280-B937-4072DA144674_cgx2pv.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1652894320/5003DFBE-D725-4CED-B939-A23AC05F41A4_itc1ih.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1652894321/BD87A80A-F249-43B0-83DE-9F93A68FD0B5_jdbbij.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1652894321/E9A4AFDB-9C70-4DB1-B1E9-1C9C8AB40A90_zuuu8u.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1652894321/80A2FF71-7D07-44A1-8DA7-D01041E069C8_fpyoqy.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657972552/E6F7C0CE-054A-49E1-B8B2-75B2616BAFFD_tzzp4k.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657972553/0C3182C6-E3CD-4834-A303-E52763A3B677_kmz9fu.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657972553/C4C873A3-9716-4131-B471-21319FE6E086_c9c5pf.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657972553/DF9F6317-63CE-4A6E-9D8C-5F4DE312EC61_roquwx.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657972553/13121F01-0525-4F1F-A720-450501224A82_chovtf.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657972553/6AD19BB5-9246-44FE-8BA5-17899BBD2B65_bu10xd.png',
+        ]
+    },
+    {
+        id: "ArkanFont",
+        title: "ArkanFont",
+        description: 'A full stack web app for arkan fonts',
+        image: 'https://res.cloudinary.com/ahmacloud/image/upload/v1657973754/portfolio/Screenshot_from_2022-07-16_15-10-29_kx6svu.png',
+        web: 'https://arkanfonts.vercel.app/?sort=new&category=all',
+        github: 'https://github.com/ahmadsoran/arkan-font-web-react',
+        longDescription: 'A full stack web app for arkan fonts, you can download your favorite kurdish fonts made with mern stack, ',
+        otherImages: [
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657973754/portfolio/Screenshot_from_2022-07-16_15-10-29_kx6svu.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657973754/portfolio/Screenshot_from_2022-07-16_15-11-35_cjfyw4.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657973754/portfolio/Screenshot_from_2022-07-16_15-11-58_ne07bs.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657973754/portfolio/Screenshot_from_2022-07-16_15-10-55_molrbt.png'
+        ]
+    },
+    {
+        id: "ArkanFontDashboard",
+        title: "ArkanFont Dashboard",
+        description: 'Dashboard for arkan fonts',
+        image: 'https://res.cloudinary.com/ahmacloud/image/upload/v1657974208/portfolio/Screenshot_from_2022-07-16_15-20-25_gxz3od.png',
+        web: '',
+        github: '',
+        longDescription: 'Dashboard for arkan fonts, you can make crud operations on your fonts file & admins users only by admin role, you can see visitors & downloded times & bug reports & uploaded fonts,  system error & info logs (can only be seen by admin role) i used metrial ui react free templates for this project .',
+        otherImages: [
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657974208/portfolio/Screenshot_from_2022-07-16_15-19-29_vur7az.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657974208/portfolio/Screenshot_from_2022-07-16_15-20-25_gxz3od.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657974209/portfolio/Screenshot_from_2022-07-16_15-22-46_ecvdpo.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657974209/portfolio/Screenshot_from_2022-07-16_15-22-20_hrydm8.png',
+            'https://res.cloudinary.com/ahmacloud/image/upload/v1657974208/portfolio/Screenshot_from_2022-07-16_15-21-42_uz9tie.png'
+        ]
+    },
+    {
+        id: "kurdistan-prayer-times-js-library",
+        title: "Kurdistan prayer times",
+        description: 'A javascript library for kurdistan prayer times',
+        image: 'https://res.cloudinary.com/ahmacloud/image/upload/v1656827025/portfolio/wjagbrmq47tmlxbrvaop.png',
+        web: '',
+        github: 'https://github.com/ahmadsoran/kurdistan-prayer-times-js',
+        longDescription: 'Kurdistan prayers time library to get the current day or next day or current month days prayer time for 13 kurdistan cities easy to use and setup check out the documentation on github ',
+        otherImages: [
+
+        ]
     }
 
 ].reverse();
